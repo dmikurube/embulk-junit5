4c6846ed6bbf8369d969cb9d26b2f2685fd7a0dc
Dai MIKURUBE <dmikurube@acm.org>
2024-09-10 15:51:45 +0900

    Add another test task ":embulkTest"

---
+++ src/embulkTest/java/org/embulk/junit5/example/TestExample.java
@@ -0,0 +1,12 @@
+package org.embulk.junit5.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestExample {
+    @Test
+    public void testExample() {
+        assertEquals(0, 1 - 1);
+    }
+}
